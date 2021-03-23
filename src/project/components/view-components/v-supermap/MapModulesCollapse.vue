a7711b1490ab5e5187906a64e5cbfaa1f24fa61a
xizher <xizher@163.com>
2021-03-23 16:42:52 +0800

    feat: map3dTile插件响应式钩子集成测试

--- src/project/components/view-components/v-supermap/MapModulesCollapse.vue
+++ src/project/components/view-components/v-supermap/MapModulesCollapse.vue
@@ -4,6 +4,15 @@
       <el-collapse-item title="底图控制">
         <BasemapControl />
       </el-collapse-item>
+      <el-collapse-item title="实体控制">
+        实体s
+      </el-collapse-item>
+      <el-collapse-item title="3dTile控制">
+        <Map3dTileControl />
+      </el-collapse-item>
+      <el-collapse-item title="GeoJSON控制">
+        GeoJSONs
+      </el-collapse-item>
     </el-collapse>
   </div>
 </template>
@@ -11,11 +20,13 @@
 <script>
 import { defineComponent } from 'vue'
 import BasemapControl from './map-modules-collapse-items/BasemapControl.vue'
+import Map3dTileControl from './map-modules-collapse-items/Map3dTileControl.vue'
 
 export default defineComponent({
   name: 'MapModulesCollapse',
   components: {
     BasemapControl,
+    Map3dTileControl,
   },
   setup () {
 
---
+++ src/project/components/view-components/v-supermap/map-modules-collapse-items/Map3dTileControl.vue
@@ -0,0 +1,39 @@
+<template>
+  <el-card
+    v-for="item in tilesetList"
+    :key="item.name"
+    class="map-3d-tile-control"
+  >
+    <div class="name">
+      {{ item.name }}
+    </div>
+    <el-switch v-model="item.visible" />
+  </el-card>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import useMap from '../../../../hooks/webmap/useSupermapMap'
+import useMap3dTile from '../../../../../zhd/dist/gis/supermap/hooks/map-3d-tile.hook'
+
+export default defineComponent({
+  name: 'Map3dTileControl',
+  setup () {
+    const [webMap] = useMap()
+    const [tilesetList] = useMap3dTile(webMap.map3dTile)
+
+    return {
+      tilesetList
+    }
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.map-3d-tile-control {
+  display: flex;
+  :deep(.el-card) {
+    width: 100%;
+  }
+}
+</style>
